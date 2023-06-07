42bb42b390b4dc9ffbfd3951a9eaaf13467a4c4b
Abaljerind <abaljerinda@gmail.com>
2023-06-07 15:33:11 +0700

    add for loop for every dropdown

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 // create function so the answer will show up when the svg clicked
-const dropdown = document.querySelector(".dropdown");
-const answer = document.querySelector(".answer");
-const question = document.querySelector(".question");
+const dropdown = document.getElementsByClassName("dropdown");
+const answer = document.getElementsByClassName("answer");
+const question = document.getElementsByClassName("question");
 
-dropdown.addEventListener("click", function () {
-  if (answer.classList.contains("hidden")) {
-    answer.classList.remove("hidden");
-    dropdown.classList.add("rotate");
-  } else {
-    answer.classList.add("hidden");
-    dropdown.classList.remove("rotate");
-  }
-  question.classList.toggle("bold-active-state");
-});
+for (let i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    if (answer[i].classList.contains("hidden")) {
+      answer[i].classList.remove("hidden");
+      dropdown[i].classList.add("rotate");
+    } else {
+      answer[i].classList.add("hidden");
+      dropdown[i].classList.remove("rotate");
+    }
+    question[i].classList.toggle("bold-active-state");
+  });
+}
 
 // close dropdown outside outside the arrow
-window.addEventListener("click", function (e) {
-  if (e.target != dropdown) {
-    dropdown.classList.remove("rotate");
-    answer.classList.add("hidden");
-  }
-});
+// window.addEventListener("click", function (e) {
+//   if (e.target != dropdown) {
+//     dropdown.classList.remove("rotate");
+//     answer.classList.add("hidden");
+//     question.classList.remove("bold-active-state");
+//   }
+// });
