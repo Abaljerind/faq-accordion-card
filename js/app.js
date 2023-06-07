7b8a8071541ed9f66538da01ef5ead1c276d33c8
Abaljerind <abaljerinda@gmail.com>
2023-06-07 22:05:05 +0700

    add fadeToggle using jquery

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 // create function so the answer will show up when the svg clicked
-const dropdown = document.getElementsByClassName("dropdown");
-const answer = document.getElementsByClassName("answer");
-const question = document.getElementsByClassName("question");
+// const dropdown = document.getElementsByClassName("dropdown");
+// const answer = document.getElementsByClassName("answer");
+// const question = document.getElementsByClassName("question");
 
-for (let i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function () {
-    if (answer[i].classList.contains("hidden")) {
-      answer[i].classList.remove("hidden");
-      dropdown[i].classList.add("rotate");
-    } else {
-      answer[i].classList.add("hidden");
-      dropdown[i].classList.remove("rotate");
-    }
-    question[i].classList.toggle("bold-active-state");
-  });
+// for (let i = 0; i < dropdown.length; i++) {
+//   dropdown[i].addEventListener("click", function () {
+//     if (answer[i].classList.contains("hidden")) {
+//       answer[i].classList.remove("hidden");
+//       dropdown[i].classList.add("rotate");
+//     } else {
+//       answer[i].classList.add("hidden");
+//       dropdown[i].classList.remove("rotate");
+//     }
+//     question[i].classList.toggle("bold-active-state");
+//   });
+// }
+
+// same with the code above but using jquery
+for (let i = 0; i < $(".dropdown").length; i++) {
+  (function (index) {
+    $(".dropdown")
+      .eq(index)
+      .on("click", function () {
+        $(".answer").eq(index).fadeToggle("slow", "linear");
+      });
+  })(i);
 }
 
 // close dropdown outside outside the arrow
